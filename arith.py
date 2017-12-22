ebbf7c7b56ff7a55a606ceaa8800fdbe3685b13f
damiano.fossa <d.fossa.91@gmail.com>
2017-12-22 10:45:35 +0100

    new functions

--- arith.py
+++ arith.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+from itertools import groupby
+
+###################################
+# P09 Pack consecutive duplicates of list elements into sublists.
+def pack(l):
+	newList = []
+	for k, g in groupby(l):
+		newList.append(list(g))
+
+	return newList
+###################################
 
 # P31 Determine whether a given integer number is prime.
 def isPrime(n):
@@ -47,11 +58,40 @@ def phi(num):
 
 # P35 Determine the prime factors of a given positive integer. (To be improved)
 def primeFactors(num):
+	primeFactors = []
+
+	if (isPrime(num)):
+		primeFactors.append(n)
+		return primeFactors
+
+	for i in range(2, num):
+		while (isPrime(i) and num % i == 0):
+				primeFactors.append(i)
+				num = num/i
+
+	return primeFactors
+
+
+# P36 Determine the prime factors of a given positive integer
+def primeFactorsMult(num):
+	primeFactorsList = primeFactors(num)
+	packList = pack(primeFactorsList)
 	newList = []
-	for i in range(1, num):
+
+	for item in packList:
+		length = len(item)
+		l = (item[0], length)
+		newList.append(l)
+
+	return newList
+
+# P39 A list of prime numbers.
+# Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
+def isPrimeInterval(x, y):
+	newList = []
+	for i in range(x, y+1):
 		if isPrime(i):
-			if num % i == 0:
-				newList.append(i)
+			newList.append(i)
 	return newList
 
 
@@ -71,4 +111,13 @@ if __name__ == "__main__":
 
 	z = 315
 	print("primeFactors " + str(z) + ": " + str(primeFactors(z)))
+	print("\n")
+
+	z = 315
+	print("primeFactorsMult " + str(z) + ": " + str(primeFactorsMult(z)))
+	print("\n")
+
+	n = 100
+	z = 215
+	print("isPrimeInterval " + str(n) + " " + str(z) + ": " + str(isPrimeInterval(n,z)))
 	print("\n")
